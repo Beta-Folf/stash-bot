65606eb4fa8b55bc6a7bd77026a9c14e4330644d
Beta Folf <beta@betafolf.dev>
2022-01-21 17:51:04 -0600

    docs: remove comments

--- bot/src/utils/ids.ts
+++ bot/src/utils/ids.ts
@@ -28,7 +28,6 @@ export async function getUserIDFromMention({
     return null;
   }
 
-  // Check length
   return valueWithoutDecorations;
 }
 export async function getChannelIDFromLink({
@@ -54,6 +53,5 @@ export async function getChannelIDFromLink({
     return null;
   }
 
-  // Check length
   return valueWithoutDecorations;
 }
