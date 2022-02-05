9fd9d89592e18e7b7451c9fd929fff6e2daba29a
Beta Folf <beta@betafolf.dev>
2022-02-05 10:30:03 -0600

    fix: update disboard logging

--- bot/src/cogs/events/Bump.event.ts
+++ bot/src/cogs/events/Bump.event.ts
@@ -5,6 +5,7 @@ import { messageStartsWithPrefix } from "~/framework/commandHandler";
 import { prisma } from "~/utils/db";
 import { USERS } from "~/constants/users";
 
+const SUCCESS_MESSAGE_REGEX = /<@[0-9]{18}> Bump done! :thumbsup:/gm;
 export default class MessageEvent extends EventCog {
   constructor(client: Client) {
     super({
@@ -17,8 +18,19 @@ export default class MessageEvent extends EventCog {
     const message = context as unknown as Message;
     const { channelId, author, embeds } = message;
 
-    if (author.id === USERS.BUMP_BOT) {
-      console.log({ embeds });
+    if (author.id !== USERS.BUMP_BOT) {
+      return;
+    }
+
+    const embed = embeds[0];
+    const { description } = embed;
+
+    console.log({ description });
+
+    if (description?.match(SUCCESS_MESSAGE_REGEX)) {
+      console.log("Matches");
+    } else {
+      console.log("Does not match");
     }
   }
 }
