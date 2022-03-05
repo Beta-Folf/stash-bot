cb18679f3bdae159b3327bde1d9fbb89e447a746
PapyrusCompendium <Poof@foxmanager.pw>
2022-03-05 01:21:04 -0500

    feat: ban users younger than 48hrs
    Not tested, should ban user accounts younger than 48hrs.

---
+++ bot/src/cogs/events/UserJoin.event.ts
@@ -0,0 +1,27 @@
+import { Client, GuildMember, Message } from "discord.js";
+import { DateTime } from "luxon";
+
+import { EventCog, EventHandlerArgs } from "~/framework/EventCog";
+
+export default class MessageEvent extends EventCog {
+  constructor(client: Client) {
+    super({
+      client,
+      eventName: "guidMemberAdd",
+    });
+  }
+
+  async eventHandler({ client, context }: EventHandlerArgs) {
+    const guildUser = context as unknown as GuildMember;
+    if (guildUser.user.bot) {
+      return;
+    }
+
+    const now = DateTime.now();
+    const userCreated = guildUser.user.createdAt;
+
+    if (now.minus({ day: 2 }).toMillis() < userCreated.getTime()) {
+      await guildUser.ban();
+    }
+  }
+}
